916b6b9e3b83aa8b472695efa23264c635a3d8ff
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-04-13 18:48:16 +0530

    Update WikipediaSearch.jsx

--- src/components/WikipediaSearch.jsx
+++ src/components/WikipediaSearch.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function WikipediaSearch() {
   return (
-    <section id="wiki" className="card" style={{ marginTop: 12 }}>
+    <section id="wiki" className="wiki-card">
       <form
         action="https://en.wikipedia.org/wiki/Special:Search"
         method="get"
@@ -13,24 +13,9 @@ function WikipediaSearch() {
           type="text"
           name="search"
           placeholder="Enter topic..."
-          style={{
-            padding: 6,
-            width: "70%",
-            border: "1px solid #d1d5db",
-            borderRadius: 6,
-          }}
+          className="wiki-input"
         />
-        <button
-          type="submit"
-          style={{
-            marginLeft: 8,
-            padding: "6px 12px",
-            background: "#2563eb",
-            color: "#fff",
-            border: "none",
-            borderRadius: 4,
-          }}
-        >
+        <button type="submit" className="wiki-btn">
           Search
         </button>
       </form>
